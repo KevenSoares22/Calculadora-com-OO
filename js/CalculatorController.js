5aac430279c5ac7c6cb62645861b2379f1ecaa77
KevenSoares22 <keven.v.soares@gmail.com>
2022-12-03 17:56:08 -0300

    Calculadora 2.0

--- js/CalculatorController.js
+++ js/CalculatorController.js
@@ -1,6 +1,49 @@
 class CalculatorController{
+    constructor(){
+        this._hourEl = document.querySelector('#hour')
+        this._dateEl = document.querySelector('#date')
+
+        this.displayHour
+        this.displayDate
+        this.currentTime
+        this.initialize()
+
+    }
+
+
+    initialize(){
+        this.calculateTime()
+        setInterval(()=>{this.calculateTime()}, 1000)
 
 
 
-    
+
+    }
+    calculateTime(){
+        this.displayHour = this.currentTime.toLocaleTimeString('pt-BR')
+        this.displayDate = this.currentTime.toLocaleDateString('pt-BR')
+
+
+    }
+    get currentTime(){
+        return new Date()
+    }
+
+    get displayHour(){
+        return this._hourEl.innerHTML
+    }
+    set displayHour(hour){
+        this._hourEl.innerHTML = hour
+    }
+
+    get displayDate(){
+        return this._dateEl.innerHTML
+    }
+    set displayDate(date){
+        this._dateEl.innerHTML = date
+    }
+
+
+
+
 }
